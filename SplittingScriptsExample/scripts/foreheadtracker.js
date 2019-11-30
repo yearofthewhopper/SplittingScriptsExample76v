fa1c8b36256b63043fd6ade2a77ee2d68bef5c61
Noland Chaliha <8598597+yearofthewhopper@users.noreply.github.com>
2019-11-30 13:14:53 -0800

    cleanupabit

--- SplittingScriptsExample/scripts/foreheadtracker.js
+++ SplittingScriptsExample/scripts/foreheadtracker.js
@@ -7,17 +7,16 @@ export const { forehead, cameraTransform } = Ft.face(0);
 
 export const foreheadTransform = cameraTransform.applyTo(forehead.center).add(R.pack3(0,0,-53));
 
-
-
 export const object = S.root.find('plane0');
+///other things you could export
 
-const driverParameters = {
-  durationMilliseconds: 2500.0,
-  loopCount: Infinity,
-  mirror: true
-};
+// export const driverParameters = {
+//   durationMilliseconds: 2500.0,
+//   loopCount: Infinity,
+//   mirror: true
+// };
 
-export const driver = A.timeDriver(driverParameters);
+// export const driver = A.timeDriver(driverParameters);
 
 
 
